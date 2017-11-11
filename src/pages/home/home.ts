b6265398a2afdff7ef96144421e869554d3f022a
OMAR HEGAZI <edgeofdarkness_x1@hotmail.com>
2017-11-11 19:42:39 +0200

    fixing some files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -28,7 +28,6 @@ export class HomePage {
           // streetViewControl: false,
           mapTypeId: 'terrain'  
         };
-<<<<<<< HEAD
 
         const map = new google.maps.Map(this.mapRef.nativeElement,options);
 
@@ -39,18 +38,6 @@ export class HomePage {
         this.addMarker(location, map);
     }
 
-=======
-
-        const map = new google.maps.Map(this.mapRef.nativeElement,options);
-
-        // set timeout to change the map type 
-        setTimeout(() => map.setMapTypeId('satellite'), 3000);
-
-        // display marker
-        this.addMarker(location, map);
-    }
-
->>>>>>> 37f78af7b855d0dd9b1a66a2e6e8e572d4425c23
     //marker function
     addMarker(position, map) {
       return new google.maps.Marker({
