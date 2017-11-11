dd90ff5828e69f167824c812634909b3f682b4b3
OMAR HEGAZI <edgeofdarkness_x1@hotmail.com>
2017-11-11 19:02:22 +0200

    added more files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// const KEY = '';
 @NgModule({
   declarations: [
     MyApp,
