fedeb437985f206bccca05b1f70dcd78478144d0
OMAR HEGAZI <edgeofdarkness_x1@hotmail.com>
2017-11-11 19:28:26 +0200

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
 
-  }
+  constructor(public navCtrl: NavController) {}
 
+    ionViewDidLoad() {
+      this.showMap();
+    }
+
+    showMap() {
+        // loaction lat long
+        const location = new google.maps.LatLng(51.55316, -0.123258); 
+            
+        // map options
+        const options = {
+          center: location,
+          zoom: 15,
+          // streetViewControl: false,
+          mapTypeId: 'terrain'  
+        };
+
+        const map = new google.maps.Map(this.mapRef.nativeElement,options);
+
+        // set timeout to change the map type 
+        setTimeout(() => map.setMapTypeId('satellite'), 3000);
+
+        // display marker
+        this.addMarker(location, map);
+    }
+
+    //marker function
+    addMarker(position, map) {
+      return new google.maps.Marker({
+        position,
+        map
+      });
+    }
 }
+
